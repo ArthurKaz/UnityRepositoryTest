7d16295cbd997b629a4daa0cf317a24022f97673
ArthurKaz <kazuk.artur@gmail.com>
2023-01-06 15:34:35 +0200

    Tst2

---
+++ My project/Assets/tst2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tst2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Feature");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
